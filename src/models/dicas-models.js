c898e267b58abc11ac7da25faa302e1e7f61554e
carolinadesouzasilva <carolsouza21@gmail.com>
2022-07-21 18:08:54 -0300

    Alterações no arquivo dicas-models.js

--- src/models/dicas-models.js
+++ src/models/dicas-models.js
@@ -1,6 +1,7 @@
-export default class DicaModels {
+class DicaModels {
     constructor (autor, dica){
         this.autor = autor;
         this.dica = dica;
     }
 }
+export default new DicaModels
---
+++ src/services/validacao-service.js
@@ -0,0 +1,20 @@
+class ValidacaoService {
+    validaAutor(autor) {
+        if (autor !== "") {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+class ValidacaoService {
+    validaAutor(dica) {
+        if (autor !== "") {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+export default new ValidacaoService;
