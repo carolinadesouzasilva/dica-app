0fc18804bdd7fcdfa982ce535b8fa78a3e9f7c3d
carolinadesouzasilva <carolsouza21@gmail.com>
2022-07-21 12:47:59 -0300

    Criada a rota no método POST. Arquivo dicas-controllers.js

--- src/controllers/dicas-controllers.js
+++ src/controllers/dicas-controllers.js
@@ -3,6 +3,10 @@ class DicasControllers {
         app.get('/',  (req, res) => {
           res.send('<h1>Deu certo<h1/>')  
         })
+
+        app.post('/', (req,res) => {
+            res.status(200).json({nome : 'Carolina'})
+        })
     }
 }
 export default new DicasControllers
